0e7272794dc1a0db4b4bf74ceb99f8972d9f5aab
wangjiansheng21 <1823079199@qq.com>
2020-12-05 17:20:21 +0800

    12

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/> 
+	<a-button type="primary">a-button</a-button>
   </div>
 </template>
 
@@ -22,7 +23,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #10151a;
   margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
+import Button from 'ant-design-vue/lib/button';
+import 'ant-design-vue/dist/antd.css';
 import App from './App.vue'
-
+Vue.component(Button.name, Button);
 Vue.config.productionTip = false
 
 new Vue({
